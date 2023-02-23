1faf57d2ff41ad0aae331bffb63ddedf206db4ad
arturoEstrada45 <84112267+arturoEstrada45@users.noreply.github.com>
2023-02-23 12:43:12 -0600

    Muchos cambios

--- vistas/dashboard.php
+++ vistas/dashboard.php
@@ -13,14 +13,14 @@ $estado = "0";
 
 $conexion = mysqli_connect('localhost', 'root', '', 'intercartonpruebas');
 
-
+//Esto nos sirve para los datos de las notificaciones
 $sql =  "SELECT * from notificaciones WHERE estado='No leido' ORDER BY notificacionID DESC";
 $resultNotis = mysqli_query($conexion, $sql);
 $numeroNotis= mysqli_num_rows($resultNotis);
 
 
 
-
+//Para la información del usuario que tiene su sesión iniciada
 $sql =  "SELECT * from empleados WHERE empleadoID='$correo'";
 $result = mysqli_query($conexion, $sql);
 
@@ -29,17 +29,18 @@ while ($mostrar = mysqli_fetch_array($result))
         $nombre=$mostrar['nombre'];      
    
 }
+//Para  sacar los tickets que se encuentran activos (Que no se les ha dado una solucion)
 $activo="Activo";
 $sqlActivos =  "SELECT * from tickets WHERE estado='$activo'";
 $result = mysqli_query($conexion, $sqlActivos);
 $numeroDatosActivos= mysqli_num_rows($result);
-
+//Tickets que no han tenido una solución
 $concluido="Corregido";
 $sqlActivos =  "SELECT * from tickets WHERE estado='$concluido'";
 $result = mysqli_query($conexion, $sqlActivos);
 $numeroConcluido= mysqli_num_rows($result);
 
-
+//tickets en general
 $sqlActivos =  "SELECT * from tickets";
 $result = mysqli_query($conexion, $sqlActivos);
 
@@ -52,11 +53,13 @@ $cuenta=0;
     $cuenta=round($cuenta,0);
 }
 
+//Esto nos sirve para extraer el mes de cada tickets,
+// esto para mostrar un ranking de los meses en los que se registran más tickets
 $numeroTotal= mysqli_num_rows($result);
 $arregloConteoMeses=array();
 $SQLFechas =  "SELECT EXTRACT(MONTH FROM fechaAlta) as fecha from tickets";
 $consultaFechas = mysqli_query($conexion, $SQLFechas);
-
+//Estas variables seran enviadas a un script el cual esta en js/demo/chart-area-demo.js 
 $enero=0;
 $febrero=0;
 $marzo=0;
@@ -69,12 +72,13 @@ $septiembre=0;
 $octubre=0;
 $noviembre=0;
 $diciembre=0;
+//La consulta de los meses es almacenada en un arreglo
 while ($datos = mysqli_fetch_array($consultaFechas)) 
 {
         array_push($arregloConteoMeses,$datos['fecha']);  
 
 }
-
+//El arreglo es descompuesto para saber el mes en el que se mandaron más tickets
 foreach($arregloConteoMeses as $meses){
         if($meses==1){
         $enero=$enero+1;
@@ -111,16 +115,43 @@ foreach($arregloConteoMeses as $meses){
         
     } if($meses==12){
         $diciembre=$diciembre+1;
-        
     }
 }
+
+//Estas acciones nos sirven para analizar a los usuarios que más tickets mandan
+$arregloUsuariosActivos=array();
+$arregloUsuariosConteo=array();
+$sqlUsuariosActivos="SELECT solicitanteID,COUNT(*) as total FROM tickets GROUP BY solicitanteID";
+$resultadoUsuariosActivos=mysqli_query($conexion,$sqlUsuariosActivos);
+
+while ($datos = mysqli_fetch_array($resultadoUsuariosActivos)) 
+{
+        array_push($arregloUsuariosActivos,$datos['solicitanteID']);
+        array_push($arregloUsuariosConteo,$datos['total']);
+}
+//foreach()
+$longitud = count($arregloUsuariosActivos);
+for ($i = 0; $i < $longitud; $i++) {
+    for ($j = 0; $j < $longitud - 1; $j++) {
+        if ($arregloUsuariosActivos[$j] > $arregloUsuariosActivos[$j + 1]) {
+            $temporal = $arregloUsuariosActivos[$j];
+            $temporal2 = $arregloUsuariosConteo[$j];
+            $arregloUsuariosActivos[$j] = $arregloUsuariosActivos[$j + 1];
+            $arregloUsuariosConteo[$j] = $arregloUsuariosConteo[$j + 1];
+            $arregloUsuariosActivos[$j + 1] = $temporal;
+            $arregloUsuariosConteo[$j + 1] = $temporal2;
+        }
+    }
+}
+
+
+
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -424,20 +455,7 @@ foreach($arregloConteoMeses as $meses){
                                 <div
                                     class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                                     <h6 class="m-0 font-weight-bold text-primary">Tickets ingresados por mes</h6>
-                                    <div class="dropdown no-arrow">
-                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                                            aria-labelledby="dropdownMenuLink">
-                                            <div class="dropdown-header">Dropdown hola:</div>
-                                            <a class="dropdown-item" href="#">Action</a>
-                                            <a class="dropdown-item" href="#">Another action</a>
-                                            <div class="dropdown-divider"></div>
-                                            <a class="dropdown-item" href="#">Something else here</a>
-                                        </div>
-                                    </div>
+                                    
                                 </div>
                                 <!-- Card Body -->
                                 <div class="card-body">
@@ -512,109 +530,26 @@ foreach($arregloConteoMeses as $meses){
                             <!-- Project Card Example -->
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Projects</h6>
+                                    <h6 class="m-0 font-weight-bold text-primary">Usuarios con más tickets</h6>
                                 </div>
+                                <?php if($longitud>0){
+                                    $aux=1;
+                                    for($i=0;$i<$longitud;$i++){
+                                        if($aux<=5){
+                                        ?>
                                 <div class="card-body">
-                                    <h4 class="small font-weight-bold">Server Migration <span
-                                            class="float-right">100%</span></h4>
+                                    <h4 class="small font-weight-bold"><?php echo $arregloUsuariosActivos[$i]?><span
+                                            class="float-right"><?php echo $arregloUsuariosConteo[$i]?></span></h4>
                                     <div class="progress mb-4">
-                                        <div class="progress-bar bg-danger" role="progressbar" style="width: 100%"
+                                        <div class="btn-outline-success" role="progressbar" style="width: 100%"
                                             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
-                                    <h4 class="small font-weight-bold">Sales Tracking <span
-                                            class="float-right">40%</span></h4>
-                                    <div class="progress mb-4">
-                                        <div class="progress-bar bg-warning" role="progressbar" style="width: 40%"
-                                            aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
-                                    <h4 class="small font-weight-bold">Customer Database <span
-                                            class="float-right">60%</span></h4>
-                                    <div class="progress mb-4">
-                                        <div class="progress-bar" role="progressbar" style="width: 60%"
-                                            aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                    <h4 class="small font-weight-bold">Payout Details <span
-                                            class="float-right">80%</span></h4>
-                                    <div class="progress mb-4">
-                                        <div class="progress-bar bg-info" role="progressbar" style="width: 80%"
-                                            aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                    <h4 class="small font-weight-bold">Account Setup <span
-                                            class="float-right">Complete!</span></h4>
-                                    <div class="progress">
-                                        <div class="progress-bar bg-success" role="progressbar" style="width: 100%"
-                                            aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                </div>
+                                    <?php $aux=$aux+1;}}}?>
                             </div>
 
                             <!-- Color System -->
-                            <div class="row">
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-primary text-white shadow">
-                                        <div class="card-body">
-                                            Primary
-                                            <div class="text-white-50 small">#4e73df</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-success text-white shadow">
-                                        <div class="card-body">
-                                            Success
-                                            <div class="text-white-50 small">#1cc88a</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-info text-white shadow">
-                                        <div class="card-body">
-                                            Info
-                                            <div class="text-white-50 small">#36b9cc</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-warning text-white shadow">
-                                        <div class="card-body">
-                                            Warning
-                                            <div class="text-white-50 small">#f6c23e</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-danger text-white shadow">
-                                        <div class="card-body">
-                                            Danger
-                                            <div class="text-white-50 small">#e74a3b</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-secondary text-white shadow">
-                                        <div class="card-body">
-                                            Secondary
-                                            <div class="text-white-50 small">#858796</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-light text-black shadow">
-                                        <div class="card-body">
-                                            Light
-                                            <div class="text-black-50 small">#f8f9fc</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-dark text-white shadow">
-                                        <div class="card-body">
-                                            Dark
-                                            <div class="text-white-50 small">#5a5c69</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            
 
                         </div>
 
@@ -623,35 +558,22 @@ foreach($arregloConteoMeses as $meses){
                             <!-- Illustrations -->
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
+                                    <h6 class="m-0 font-weight-bold text-primary">INTERCARTON</h6>
                                 </div>
                                 <div class="card-body">
                                     <div class="text-center">
                                         <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;"
-                                            src="../img/undraw_posting_photo.svg" alt="...">
+                                            src="../img/inter.png" alt="...">
                                     </div>
-                                    <p>Add some quality, svg illustrations to your project courtesy of <a
-                                            target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a
-                                        constantly updated collection of beautiful svg images that you can use
-                                        completely free and without attribution!</p>
-                                    <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on
-                                        unDraw &rarr;</a>
+                                    <p>En Intercarton, estamos comprometidos a la comercialización y distribución de papel y cartón plegadizo proveniente de fuentes sustentables de una manera confiable. Y a través de la mejora continua de todos nuestros procesos, basados en el cumplimiento de nuestro SGC y SG-CoC, lograremos la plena satisfacción de nuestros clientes mediante productos que cumplan sus requisitos.
+                                        
+                                    </p>
+                                    <a target="_blank" rel="nofollow" href="https://www.intercarton.com.mx/nosotros/">Nosotros &rarr;</a>
                                 </div>
                             </div>
 
                             <!-- Approach -->
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Development Approach</h6>
-                                </div>
-                                <div class="card-body">
-                                    <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce
-                                        CSS bloat and poor page performance. Custom CSS classes are used to create
-                                        custom components and custom utility classes.</p>
-                                    <p class="mb-0">Before working with this theme, you should become familiar with the
-                                        Bootstrap framework, especially the utility classes.</p>
-                                </div>
-                            </div>
+                           
 
                         </div>
                     </div>
